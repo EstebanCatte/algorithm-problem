67fc609837d6f60838d9b00ee1dad6da76d0425f
Esteban <esteban.catte@cea.fr>
2023-11-23 15:58:43 +0100

    First solution; added ICPC test; added file management

--- ICPC/attempt1.py
+++ ICPC/attempt1.py
@@ -5,7 +5,7 @@ class User():
 
     def __init__(self, id, num_rbg, num_bs, num_tti, num_frame):
         self.id = id
-        self.sinr0 = np.zeros((num_bs, num_tti, num_rbg))
+        self.sinr0 = np.zeros((num_rbg, num_tti, num_bs))
         self.status = np.zeros((num_frame, num_tti)) #connected to the 0th basestation
         self.block = 0
         self.tti = 0
@@ -41,10 +41,12 @@ class Solver():
         self.sinr = np.zeros((num_user, num_bs, num_tti, num_rbg ))
 
 
+    #determine a user is communicating with <bs> at <tti> with <rbg>
     def is_communicating(self, user, bs, rbg, tti):
         return self.powers[user.id][bs.id][tti][rbg] > 0
 
 
+    #change power value
     def allocate_power(self, power_value, user, bs, rbg, tti):
         self.powers[user.id][bs.id][rbg][tti] = np.clip(power_value, 0, 4)
 
@@ -83,19 +85,19 @@ class Solver():
                 for interf_user in self.users:
                     if interf_user != user:
                         interf_factor = interf_bs.interf_factor[interf_user.id][user.id][rbg]
-                        sinr0 = user.sinr0[interf_bs.id][tti][rbg]
+                        sinr0 = user.sinr0[rbg][tti][interf_bs.id]
                         power = self.powers[interf_user.id][interf_bs.id][tti][rbg]
                         interference +=  sinr0 * power * math.exp(-interf_factor)
         return interference
 
 
     def compute_sinr(self, user , bs, tti, rbg):
-        sinr0 = user.sinr0[bs.id][tti][rbg]
+        sinr0 = user.sinr0[rbg][tti][bs.id]
         power = self.powers[user.id][bs.id][tti][rbg]
         in_interference = self.compute_in_interference(user, bs, tti, rbg)
         out_interference = self.compute_out_interference(user, bs, tti, rbg)
         sinr =  (sinr0 * power * in_interference) / (1+out_interference)
-        print("Com User {} bs {} in_interf = {} et out_interf = {}".format(user.id, bs.id, in_interference, out_interference))
+        #print("Com User {} bs {} in_interf = {} et out_interf = {}".format(user.id, bs.id, in_interference, out_interference))
         self.sinr[user.id][bs.id][tti][rbg] = sinr
         return sinr
 
@@ -125,7 +127,7 @@ class Solver():
 
 
     def check_frame_validity(self, frame):
-        return frame["rate"] > frame["TBS"]
+        return frame["rate"] >= frame["TBS"]
 
 
     #Met bien en forme l'input
@@ -135,7 +137,7 @@ class Solver():
             for b in range(self.num_bs):
                 for r in range(self.num_rbg):
                     for i, user in enumerate(self.users):
-                        user.sinr0[b][t][r] = sinr[r + b*num_rbg + t*num_bs*num_rbg][i]
+                        user.sinr0[r][t][b] = sinr[r + b*num_rbg + t*num_bs*num_rbg][i]
 
     
     def formate_interf(self, interference):
@@ -155,116 +157,159 @@ class Solver():
                     for r in range(self.num_rbg):
                         p = self.powers[u.id][bs.id][t][r]
                         sinr = self.sinr[u.id][bs.id][t][r]
-                        if p > 0:
-                            print("BS {} sending to UE {} with block {} and TTI {}".format(bs.id, u.id, t, r))
-                        print("===SINR === BS {} sending to UE {} with block {} and TTI {} = {}".format(bs.id, u.id, t, r, sinr))
+                        # if p > 0:
+                        #     print("BS {} sending to UE {} with block {} and TTI {}".format(bs.id, u.id, t, r))
+                        # print("===SINR === BS {} sending to UE {} with block {} and TTI {} = {}".format(bs.id, u.id, t, r, sinr))
 
+
+    def sort_frames(self, frames):
+        sorted_frames = sorted(frames, key=lambda x: x['TBS'])
+        return sorted_frames
+
+
+class Heuristic1(Solver):
+
+    def __init__(self, users, basestations, num_rbg, num_bs, num_user, num_tti, num_frame, frames, sinr, interference):
+        super().__init__(users, basestations, num_rbg, num_bs, num_user, num_tti, num_frame, frames, sinr, interference)
+
+
+    def compute_heuristic_matrix(self):
+        #metric is sum(sinr0)
+        self.heuristic_matrix = np.zeros((self.num_user, self.num_rbg, self.num_tti))
+        for user in self.users:
+            for r in range(self.num_rbg):
+                for t in range(self.num_tti):
+                    self.heuristic_matrix[user.id][r][t] = np.sum(user.sinr0[r][t])
+
+        return self.heuristic_matrix
+
+
+    def find_power(self, TBS, user, rbg, tti):
+        W = 192
+        #2% de marge sur le TBS
+        cons = 2**((TBS/1.98)/W) 
+        lower_sinr = np.min(user.sinr0[rbg][tti])
+        value = (cons * math.sqrt(1/(lower_sinr**2))*lower_sinr - 1) / lower_sinr
+        return value
+
+
+    def process_frame(self, frame):
+        user = self.users[frame["userid"]]
+        heuristic = self.heuristic_matrix[user.id]
+        
+        max_index_flat = np.argmax(heuristic)
+        max_index = np.unravel_index(max_index_flat, heuristic.shape)
+        
+        rbg = max_index[0]
+        tti = max_index[1]
+        self.heuristic_matrix[user.id][max_index[0]][max_index[1]] = -1e9
+        power_to_allocate = self.find_power(frame["TBS"], user, rbg, tti)
+        
+        for bs in self.basestations:
+            self.powers[user.id][bs.id][tti][rbg] = power_to_allocate
+
+
+    def print_results(self):
+        for tti in range(self.num_tti):
+            for bs in self.basestations:
+                for rbg in range(self.num_rbg):
+                    for user in self.users:
+                        print(self.powers[user.id][bs.id][tti][rbg], end=" ")
+                    print("")
 
 #==========================================================================
 #                       INPUTS INPUTS INPUTS
 #==========================================================================
-#1 TTI is 0.5ms
-#Number of USER
-#users are numbered from 0 to N-1
-#num_user = int(input())
+print("###########################################")
+print("                 ICPC")
+print("###########################################")
+with open('./tests/06.txt', 'r') as file:
+    lines = file.readlines()
+
+# # 1 TTI is 0.5ms
+# # Number of USER
+# # users are numbered from 0 to N-1
+num_user = int(lines[0].strip())
 
 
-#Number of cell
-#cell are numbered from 0 to K-1
-#each of which is equipped with a base station to serve users.
-#One base station usually serves multiple users, and multiple base stations may serve one user at the same time.
-#num_bs = int(input())
+# # Number of cell
+# # cell are numbered from 0 to K-1
+# # each of which is equipped with a base station to serve users.
+# # One base station usually serves multiple users, and multiple base stations may serve one user at the same time.
+num_bs = int(lines[1].strip())
 
 
-#TTI number
-#Transmission Time Interval
-#numbered from 0 to T-1
-#num_tti = int(input())
+# # TTI number
+# # Transmission Time Interval
+# # numbered from 0 to T-1
+num_tti = int(lines[2].strip())
 
 
-#RBD number
-#Each RBG coresponds to a transmission bandwidth of 5760 kHz
-#numbered from 0 to T-1
-#num_rbg = int(input())
+# # RBD number
+# # Each RBG coresponds to a transmission bandwidth of 5760 kHz
+# # numbered from 0 to T-1
+num_rbg = int(lines[3].strip())
 
 
-#Initial Signal-to-Interference-plus-Noise-Ratio
-# sinr = []
-# for i in range(5, 5+num_rbg*num_bs*num_tti):
-#     sinr.append(list(map(float, input().split())))
+# #Initial Signal-to-Interference-plus-Noise-Ratio
+sinr = []
+for i in range(4, 4+num_rbg*num_bs*num_tti):
+    sinr.append(list(map(float, lines[i].strip().split())))
 
 
-#interference factor
-# interference = []
-# for i in range(5+num_rbg*num_bs*num_tti, 5+num_rbg*num_bs*num_tti + num_user*num_rbg*num_bs):
-#     interference.append(list(map(float, input().split())))
+# interference factor
+interference = []
+for i in range(4+num_rbg*num_bs*num_tti, 4+num_rbg*num_bs*num_tti + num_user*num_rbg*num_bs):
+    interference.append(list(map(float, lines[i].strip().split())))
 
 
-#Number of frame
-# num_frame = int(input())
-# frame = []
-# for i in range(num_frame):
-#     inp = list(map(float, input().split()))
-#     frame.append({"frameid": inp[0], #frame id 0 to J-1 increasing order
-#                 "TBS": inp[1], #size TBS
-#                 "userid": inp[2], #user ID it belongs to
-#                 "t0": inp[3], #first tti from 0 to T-1
-#                 "tti": inp[4], #number of TTI
-#                 "t1": inp[3]+inp[4]}) 
+# # Number of frame
+num_frame = int(lines[4+num_rbg*num_bs*num_tti+num_user*num_rbg*num_bs].strip())
+frames = []
+for i in range(num_frame):
+    inp = list(map(float, lines[4+num_rbg*num_bs*num_tti+num_user*num_rbg*num_bs+1+i].split()))
+    frames.append({"frameid": int(inp[0]), #frame id 0 to J-1 increasing order
+                "TBS": int(inp[1]), #size TBS
+                "userid": int(inp[2]), #user ID it belongs to
+                "t0": int(inp[3]), #first tti from 0 to T-1
+                "tti": int(inp[4]), #number of TTI
+                "t1": int(inp[3]+inp[4])}) 
 
 #==========================================================================
 #                       TEST
 #==========================================================================
-frames = []
-num_user = 2
-num_bs = 2
-num_tti = 2
-num_rbg = 1
-sinr = [[1.38, 11.38], [1.38, 11.38], [2.38, 2.38], [2.38, 2.38]]
-interference = [[0,-2], [-2, 0], [0, -2], [-2, 0]]
-num_frame = 2
-inpu = ["0 250 0 0 2", "1 25 1 0 2"]
-for i in range(num_frame):
-    inp = list(map(float, inpu[i].split()))
-    #cette ligne peut ne pas avoir de sens
-    if inp[4] > 1: 
-        frames.append({"frameid": int(inp[0]), #frame id 0 to J-1 increasing order
-                    "TBS": inp[1], #size TBS
-                    "userid": int(inp[2]), #user ID it belongs to
-                    "t0": inp[3], #first tti from 0 to T-1
-                    "tti": inp[4], #number of TTI
-                    "t1": inp[3]+inp[4]-1,
-                    "rate":0}) 
+print("#  NUM UE : {}".format(num_user))
+print("#  NUM BS : {}".format(num_bs))
+print("#  NUM TTI: {}".format(num_tti))
+print("#  NUM RBG: {}".format(num_rbg))
+print("#  NUM FRA: {}".format(num_frame))
+print("#############################")
+
 #==========================================================================
 #                       Start
 #==========================================================================
 users = [User(i, num_rbg, num_bs, num_tti, num_frame) for i in range(num_user)]
 basestations = [Basestation(i, num_user, num_rbg) for i in range(num_bs)]
 
-solver = Solver(users, basestations, num_rbg, num_bs, num_user, num_tti, num_frame, frames, sinr, interference)
+solver = Heuristic1(users, basestations, num_rbg, num_bs, num_user, num_tti, num_frame, frames, sinr, interference)
 
-#self.powers = np.zeros((num_user, num_bs, num_tti, num_rbg ))
-solver.powers[1][0][0][0] = 0.004950
-solver.powers[1][1][0][0] = 0.004950
-solver.powers[0][0][1][0] = 0.245039
-solver.powers[0][1][1][0] = 0.245039
+#1) trier les frames
+sorted_frames = solver.sort_frames(frames)
+solver.compute_heuristic_matrix()
+solver.process_frame(sorted_frames[0])
+solver.process_frame(sorted_frames[1])
 
-solver.compute_rate(frames[0])
-solver.compute_rate(frames[1])
+
+for frame in sorted_frames:
+    print("Computing frame {} with TBS {}".format(frame["frameid"], frame["TBS"]))
+    solver.compute_rate(frame)
+
 for i in range(num_frame):
     print("Frame {} TBS {} Rate {}".format(i, frames[i]["TBS"], frames[i]["rate"]))
     print("Frame {} check: {}".format(i, solver.check_frame_validity(frames[0])))
 
 
-#on peut trier les frames par difficulté en terme de TBS
-
-
-#Output for a certain input is the optimization result of p(k)rnt (float), which has R⋅K⋅T
-#lines. Each line has N elements, corresponding to N users. 
-# p(k)rnt is the (n+1) -th element of line (1+r+k⋅R+t⋅K⋅R)
-# /!\ 
-#p(k)rnt is a nonnegative continuous variable denoting the power allocated to user n in the r-th RBG of cell k at TTI t
-#determiner la puissance au User N dans le RBG r de la BS k au TTI t
-# /!\ 
-
-#We have 0 < p(k)rnt < 4 and each cell must respect sum(p(k)nrt) < R
+print("###########################################")
+print("                 RESULTS")
+print("###########################################")
+solver.print_results()
